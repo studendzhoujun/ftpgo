4fb15b4fee3e94cee4f6d677f699cd5560ac02d8
zhoujun <zhoujun147@jd.com>
2020-03-06 17:24:32 +0800

    :bento: ftp

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ftpgo
+```bash
+{
+    "localPath": "./dist",
+    "remotePath": "/var/www/xxx/xxx",
+    "protectedRemotePath": "/var/www/",
+    "connect": {
+        "host": "xxx.xxx.xx.xx",
+        "port": "",
+        "username": "xxx",
+        "password": "xxx"
+    }
+}
+```
--- utils/utils.js
+++ utils/utils.js
@@ -30,11 +30,11 @@ function getFilesInfo(filePath) {
 }
 
 function getSftpConfig() {
-  const sftpPath = path.join(process.cwd(), "./sftp.json");
+  const sftpPath = path.join(process.cwd(), "./ftpconfig.json");
   if (fs.existsSync(sftpPath)) {
     return JSON.parse(fs.readFileSync(sftpPath, "utf-8"));
   } else {
-    throw new Error("当前目录下未找到 sftp.json 文件!");
+    throw new Error("当前目录下未找到 ftpconfig.json 文件!");
   }
 }
 
